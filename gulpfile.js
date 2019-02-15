df9e9a9802cd639b8284f86e58f79cc27bf753e1
吴光宇 <532619140@qq.com>
2019-02-15 14:38:47 +0800

    第四次

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,45 @@ var autoprefixer = require("gulp-autoprefixer"); // 前缀
 var clean = require("gulp-clean-css"); // 压缩
 var concat = require("gulp-concat"); // 合并
 var scss = require("gulp-sass"); // 编译scss
+var server = require("gulp-webserver")
+var url = require("url");
+var path = require("path")
+var fs = require("fs")
 
 //编译
 gulp.task("Sass", function() {
     return gulp.src(['./src/scss/*.scss', '!./src/scss/_mixin.scss', '!./src/scss/common.scss'])
         .pipe(scss())
-        .pipe(clean())
         .pipe(autoprefixer({
             browsers: ['last 2 versions']
         }))
+        .pipe(clean())
         .pipe(gulp.dest('./src/css/'))
+})
+
+//起服
+gulp.task("server", function() {
+    return gulp.src('./src')
+        .pipe(server({
+            port: 8080,
+            open: true,
+            livereload: true,
+            middleware: function(req, res, next) {
+                var pathname = url.parse(req.url).pathname;
+                console.log(pathname);
+                if (pathname === "/favicon.ico") {
+                    return res.end()
+                }
+                pathname = pathname === "/" ? "index.html" : pathname;
+                var filename = path.extname(pathname)
+                if (filename) {
+                    var filepath = path.join(__dirname, "src", pathname)
+                    if (fs.existsSync(filepath)) {
+                        res.end(fs.readFileSync(filepath))
+                    }
+                }
+            }
+        }))
 })
 
 //监听
@@ -22,4 +51,4 @@ gulp.task("watch", function() {
 
 
 
-gulp.task("dev", gulp.series("Sass", "watch"))
+gulp.task("dev", gulp.series("Sass", "server", "watch"))
